3e12ffe08c9922f2733fba80ec93b2022b625608
221geek <abdoulazizdione1997@gmail.com>
2019-10-15 17:48:42 +0000

    ma cousine d..g.n

--- controller/class/hosto.php
+++ controller/class/hosto.php
@@ -7,7 +7,7 @@ class Database
 
     public static function getPDO():PDO{
         if(self ::$pdoInstance == null){
-            self ::$pdoInstance = new PDO('mysql:host=localhost;dbname=hosto;charset=utf8', 'root', 'root',[
+            self ::$pdoInstance = new PDO('mysql:host=localhost;dbname=hosto;charset=utf8', 'root', '',[
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
                 PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION
             ]);

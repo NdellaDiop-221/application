9965cbb3cf627ce5ad9eb9890ef54d0c697f8192
221geek <abdoulazizdione1997@gmail.com>
2019-10-18 23:18:00 +0000

    fais

--- controller/ttt.php
+++ controller/ttt.php
@@ -1,46 +1,50 @@
 <?php
     
     if (isset($_POST['valider'])) {
-        if (!empty($_POST['uname']) && !empty($_POST['psw']) && !empty($_POST['choix'])) {
+        if (!empty($_POST['uname']) && !empty($_POST['psw'])) {
             
             $mail = $_POST['uname'];
             $passw = $_POST['psw'];
-            $choix = $_POST['choix'];
 
             require "class/hosto.php";
 
             $connect = Database::getPDO();
 
-            $requete = $connect->prepare('SELECT id FROM user WHERE email = :mail AND mot_de_pass = :psw');
+            $requete = $connect->prepare('SELECT * FROM users WHERE mail = :mail AND pass = :pass');
             $requete->execute(array(
-                    'mail' => $mail,
-                    'psw' => $passw
+                    'mail' => $email,
+                    'pass' => $password
                 ));
-            $exist = $requete->fetch();
+            $result = $requete->fetch();
 
-            if(!$exist){
+            if (!$result) {
                 header("Location: ../view/login.php");
                 exit;
             }
             else{
                 session_start();
+                $_SESSION['id'] = $result->{'id'};
 
-                $_SESSION['id'] = $exist->{'id'};
-                $_SESSION['mail'] = $mail;
+                $role = $result->{'id_role'};
 
-                if ($choix == "administrateur") {
-                    header("Location: ../view/administrateur/admin.php");
-                }
-                if ($choix == "secretaire") {
-                    header("Location: ../view/secretaire/secretaire.php");
-                }
-                if ($choix == "medecin") {
-                    header("Location: ../view/medecin/medecin.php");
+                switch ($role) {
+                    case 1:
+                        header("Location: ../view/administrateur/admin.php");
+                    break;
+                    case 2:
+                        header("Location: ../view/secretaire/secretaire.php");
+                    break;
+                    case 3:
+                        header("Location: ../view/medecin/medecin.php");
+                    break;
+
+                default:
+                    header("Location: ../view/login.php");
+                    exit;
                 }
             }
         }
-    }
-    else{
-        header("Location: ../view/login.php");
-        exit;
+        else{
+            /*  */
+        }
     }

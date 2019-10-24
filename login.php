12079e9226c2fe3480a99a70dcfe07597ec5ba22
Ndoumbe Diop <materflsh@gmail.com>
2019-10-24 22:53:09 +0200

    gestionrv

---
+++ login.php
@@ -0,0 +1,39 @@
+    <!DOCTYPE html>
+
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title></title>
+            <meta name="description" content="">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+        </head>
+        <body>
+        
+        <form class="dropdown-menu p-4">
+    <div class="form-group">
+        <label for="exampleDropdownFormEmail2">Email address</label>
+        <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
+    </div>
+    <div class="form-group">
+        <label for="exampleDropdownFormPassword2">Password</label>
+        <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="dropdownCheck2">
+        <label class="form-check-label" for="dropdownCheck2">
+            Remember me
+        </label>
+        </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
+    
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js" async defer></script>
+
+        </body>
+    </html>
+    
+    
+    
